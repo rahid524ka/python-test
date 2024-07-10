a23b7d44e17fcee9ab816e5c0272f20b09294c5a
Rahid khan <rahidka524@gmail.com>
2024-07-10 20:15:23 +0500

    new files

--- read.py
+++ read.py
@@ -1,8 +1,12 @@
-file = open("file.txt", "r")
+file = open("file1.txt", "w")
+# r = to read
+# a = to append
+# w = to overwrite
+# r+ = to read and overwrite
 
-for fil in file.readlines():
-    print (fil)
-
+#for fil in file.readlines():
+ #   print (fil)
+file.write("this line is add through programming")
 #print(file.read())
 #print(file.readable())
 #print(file.readline())
