d7c15de50e1a5e7cc82bc14713482b2e91fab80d
Rahid khan <rahidka524@gmail.com>
2024-07-10 19:52:09 +0500

    change in read file

--- read.py
+++ read.py
@@ -1,7 +1,10 @@
 file = open("file.txt", "r")
 
+for fil in file.readlines():
+    print (fil)
+
 #print(file.read())
-print(file.readable())
+#print(file.readable())
 #print(file.readline())
 #print(file.readlines())
 file.close()
