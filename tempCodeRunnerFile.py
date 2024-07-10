6590ab81ac340265cf65d19d34e33534cf59b367
Rahid khan <rahidka524@gmail.com>
2024-07-10 16:25:45 +0500

    adding to files

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+from math import pi
+p = input (" enter the radius of the circle ")
+
+area = p*p*pi
+
+print(" the area of the circle is {area}")
---
+++ tested.py
@@ -0,0 +1,6 @@
+from math import pi
+p = int(input(" enter the radius of the circle "))
+
+area = p*p*pi
+
+print(f" the area of the circle is {area}")
